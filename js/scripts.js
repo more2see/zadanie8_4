e204a74b426f61375e14803818d8910e3365a17d
Dorota Kazak <dorota.kazak@gmail.com>
2018-04-28 21:13:24 +0200

    Add new comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,12 @@
 //Aby sprawdzić, jaką wartość ma zmienna w danym momencie,
 
+
+var name = prompt('Enter your name');
+
 /* polecam posłużyć się wbudowaną funkcją console.log(variable name);. 
 Wyświetli ona aktualną wartość zmiennej w konsoli przeglądarki.*/
 
-//Dokładnie tak samo jak komentowaliśmy CSS'a:-)
+alert('Hello, ' + name);//Dokładnie tak samo jak komentowaliśmy CSS
 
 
-var variable = 'test';
-
-console.log(variable);
+console.log('Hello, ' + name);
