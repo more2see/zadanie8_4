debfec24f4387d3ec0a484bb64bea789dbcf33f5
Dorota Kazak <dorota.kazak@gmail.com>
2018-04-28 21:09:05 +0200

    Add comments to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Aby sprawdzić, jaką wartość ma zmienna w danym momencie,
+
+/* polecam posłużyć się wbudowaną funkcją console.log(variable name);. 
+Wyświetli ona aktualną wartość zmiennej w konsoli przeglądarki.*/
+
+//Dokładnie tak samo jak komentowaliśmy CSS'a:-)
+
+
+var variable = 'test';
+
+console.log(variable);
